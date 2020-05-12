d275775487295a457f6665c92e82f5f840d6622b
José Domingo Muñoz <josedom24@gmail.com>
2020-05-12 13:40:12 +0200

    Primer commit

---
+++ app.py
@@ -0,0 +1,68 @@
+import os
+from flask import Flask, render_template, flash, redirect, url_for, abort
+from markupsafe import escape
+from flask_bootstrap import Bootstrap
+from forms import SignupForm
+from funciones import *
+
+app = Flask(__name__)
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
+Bootstrap(app)
+
+# Our index-page just shows a quick explanation. Check out the template
+# "templates/index.html" documentation for more details.
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# Shows a long signup form, demonstrating form rendering.
+@app.route('/sistema/<sistema>/', methods=('GET', 'POST'))
+@app.route('/sistema/<sistema>/<path:ruta>', methods=('GET', 'POST'))
+def juegos(sistema,ruta=""):
+    filtro,redireccionar=getFiltro(sistema,ruta)    
+    if redireccionar:
+        return redirect(getRuta(filtro))
+    datos=allDatos(sistema.upper(),filtro)
+    
+    busqueda=getDatos(datos)
+    #print(getDatos(getGame(datos,"Racing","categoria"),"desarrollador"))
+    return render_template('juegos.html',sistema=sistema,datos=datos,busqueda=busqueda,filtro=filtro,url=getRuta(filtro))
+
+@app.route('/webmsx/<sistema>/<id>')
+def webmsx(sistema,id):
+    filtro={'sistema': sistema}
+    datos=allDatos(sistema.upper(),filtro)
+    
+    game=getGame(datos,id,"id")
+    print(game)
+    if len(game)==0:
+        abort(404)
+    if game[0]["files"][0].lower().endswith(".dsk"):
+        tipo="dsk"
+    if game[0]["files"][0].lower().endswith(".rom"):
+        tipo="rom"
+    if game[0]["files"][0].lower().endswith(".cas"):
+        tipo="cas"
+    return render_template('webmsx.html',game=game[0],tipo=tipo)
+app.run("0.0.0.0",debug=True)
+    
+
+
+#def msx():
+#    form = SignupForm()
+#
+#    if form.validate_on_submit():
+#        # We don't have anything fancy in our application, so we are just
+#        # flashing a message when a user completes the form successfully.
+#        #
+#        # Note that the default flashed messages rendering allows HTML, so
+#        # we need to escape things if we input user values:
+#        flash('Hello, {}. You have successfully signed up'
+#              .format(escape(form.name.data)))
+#
+#        # In a real application, you may wish to avoid this tedious redirect.
+#        return redirect(url_for('.index'))
+#
+#    return render_template('signup.html', form=form)
---
+++ funciones.py
@@ -0,0 +1,107 @@
+import os,json
+from operator import itemgetter
+SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+GAME_ROOT = "/home/jose/Nextcloud/Games/"
+def LeerDatos(nombre):
+    json_url = os.path.join(SITE_ROOT, 'static', nombre+".json")
+    with open(json_url) as file:
+        return json.load(file)
+
+def getGame(datos,valor,campo="nombre"):
+    try:
+        if campo=="id":
+            return [game for game in datos if game[campo] == int(valor)]
+        else:
+            return [game for game in datos if game[campo] == valor]
+    except:
+        return []
+
+def isGame(datos,valor,campo="nombre"):
+    return len(getGame(datos,valor))>0
+
+def getDatos(datos):
+    info={}
+    keys=["distribuidor","desarrollador","categoria","año"]
+    for campo in keys:
+        info[campo]=getDato(datos,campo)
+    info["letras"]=getLetra(datos)
+    return info
+
+def getDato(datos,devcampo):
+    try:
+        return sorted(list(set([str(game[devcampo]).strip() for game in datos])))
+    except:
+        return []
+
+def getLetra(datos):
+    try:
+        return sorted(list(set([game["nombre"][0].upper() for game in datos])))
+    except:
+        return []
+
+def LeerFich(nombre):
+    lista=[]
+    lista2=[]
+    for root, dirs, files in os.walk(GAME_ROOT+nombre):
+        for name in dirs:
+            if len(name)>1:
+                lista.append(os.path.join(root, name))
+        for name in files:
+            lista2.append(os.path.join(root, name))
+    return lista,lista2
+
+
+def allDatos(nombre,filtro):
+    datos=LeerDatos(nombre)
+    dirs,ficheros=LeerFich(nombre)
+    games=[]
+    for game in dirs:
+        g=getGame(datos,game.split("/")[-1])[0]
+        g["files"]=sorted(["/".join(z[4:]) for z in  [y for y in [x.split("/") for x in ficheros] if game.split("/")[-1] in y]])
+        try:
+            g["image"]=[x for x in g["files"] if ".jpg" in x or ".png" in x][0]
+            g["files"].remove(g["image"])
+        except:
+            g["image"]=""
+        cont=0
+        
+        for campo,valor in filtro.items():
+            if campo=="letras" and g["nombre"].startswith(valor):
+                cont=cont+1
+            else:
+                try:
+                    if g.get(campo).lower()==valor.lower():
+                        cont=cont+1
+                except:
+                    if str(g.get(campo))==str(valor):
+                        cont=cont+1
+        if cont==len(filtro):
+            games.append(g)
+
+    games=sorted(games, key=itemgetter('nombre'))
+    return games
+## recibe un sistema y una ruta y crea un diccionario con los campos y valores
+def getFiltro(sistema,ruta):
+    redirect=False
+    filtro={"sistema":sistema}
+    try:
+        ruta=ruta.split("/")
+    except: 
+        return filtro
+    for campo,valor in zip(ruta[::2],ruta[1::2]):
+        if campo in filtro:
+            redirect=True
+        if filtro.get(campo)==valor:
+            del filtro[campo]
+            redirect=True
+        else:
+            filtro[campo]=valor
+    return filtro,redirect
+
+def getRuta(filtro):
+    copia=filtro.copy()
+    ruta="/sistema/"+copia["sistema"]+"/"
+    del copia["sistema"]
+    for campo,valor in copia.items():
+        ruta=ruta+campo+"/"+valor+"/"
+    return ruta
