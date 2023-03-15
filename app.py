c76025fb69975a9b2b9b9799b40686708a7c536c
José Domingo Muñoz <josedom24@gmail.com>
2023-03-15 17:43:43 +0100

    Lanzador de juegos

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect, url_for
 from funciones import *
 
 
@@ -9,6 +9,9 @@ SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 app.config['SISTEMAS']=["todos","msx","msx2","amiga","mame"]
 app.config['DIR']={"msx":"Microsoft - MSX","msx2":"Microsoft - MSX2","amiga":"Commodore - Amiga","mame":"MAME"}
+with open("enlaces.json") as fichero:
+    app.config["ENLACES"]=json.load(fichero)
+
 
 
 @app.context_processor
@@ -36,7 +39,50 @@ def juegos(sistema,filtro=""):
 @app.route('/juego/<sistema>/<sistema_juego>/<nombre>', methods=('GET', 'POST'))
 def juego(sistema,sistema_juego,nombre):
     juego=LeerDatos(sistema_juego,app.config["SISTEMAS"],{"título":nombre})
-    return render_template('juego.html',sistema=sistema,game=juego["lista"][0],dir=app.config["DIR"])
+    return render_template('juego.html',sistema_juego=sistema_juego,sistema=sistema,game=juego["lista"][0],dir=app.config["DIR"])
+
+@app.route('/jugar/<sistema>/<sistema_juego>/<nombre>', methods=('GET', 'POST'))
+def jugar(sistema,sistema_juego,nombre):
+    juego=""
+    instruccion=""
+    dir_cores="~/.var/app/org.libretro.RetroArch/config/retroarch/cores/"
+    dir_files="/media/jose/copia/Juegos/roms/"
+    if sistema_juego=="msx" or sistema_juego=="msx2":
+        core="bluemsx_libretro.so"
+        extension=".zip"
+        dir=sistema_juego
+    elif sistema_juego=="amiga":
+        core="puae_libretro.so"
+        extension=".zip"
+        dir=sistema_juego+"500"
+    elif sistema_juego=="mame":
+        core="mame_libretro.so"
+        extension=".zip"
+        dir=sistema_juego
+        playlist="/media/jose/copia/Juegos/playlists/MAME.lpl"
+        with open(playlist) as fichero:
+            datos=json.load(fichero)
+        juego=LeerDatos(sistema_juego,app.config["SISTEMAS"],{"título":nombre})
+        j=[item for item in datos["items"] if item['label'] == juego["lista"][0]["fichero"]][0]
+        if j["core_name"]=="Arcade (FinalBurn Neo)":
+            core="fbneo_libretro.so"
+        if j["core_name"]=="Arcade (MAME 2003)":
+            core="mame2003_libretro.so"
+        instruccion="flatpak run --filesystem=host org.libretro.RetroArch  -L "+dir_cores+core+ ' "'+j["path"]+'"'
+
+    if juego == "":
+        juego=LeerDatos(sistema_juego,app.config["SISTEMAS"],{"título":nombre})
+    if instruccion=="":
+        instruccion="flatpak run --filesystem=host org.libretro.RetroArch  -L "+dir_cores+core+ ' "'+dir_files+dir+"/"+juego["lista"][0]["fichero"]+extension+'"'
+    
+    try:
+        os.system(instruccion)
+    except:
+        pass
+
+        
+    return redirect(url_for('juego',sistema=sistema,sistema_juego=sistema_juego,nombre=nombre))    
+
 
 app.run("0.0.0.0",debug=True)
     
